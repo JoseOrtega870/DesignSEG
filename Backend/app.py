748e9c5b94ce32f21fef82224a081cd8a0e736bc
rmrhx01 <rmrhx01@gmail.com>
2024-05-19 11:45:52 -0600

    Added GET and PUT routes for /users
    Added the mentioned routes, also cleaned the code and added the function wrapper to facilitate making queries

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,65 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import sqlite3
 import bcrypt
 
+
+app = Flask(__name__)
+database = 'database.db'
+
+# Function to decorate functions that requiere a connection to the database
+def query(database:str):
+    def wrapper(customQuery):
+        def wrapFunction(*args,**kwargs):
+            # Creates connection to the database and closes it when done
+            connection = sqlite3.connect(database)
+            cursor = connection.cursor()
+            result = customQuery(cursor,connection,*args,**kwargs)
+            cursor.close()
+            connection.close()
+            return result
+        return wrapFunction
+    return wrapper
+
+# Function to hash a password with bcrypt
+def hashPassword(passwordString:str)->str:
+    # Converting password to array of bytes 
+    bytes = passwordString.encode('utf-8') 
+    
+    # Generating the salt 
+    salt = bcrypt.gensalt() 
+    
+    # Hashing the password 
+    passwordHash = bcrypt.hashpw(bytes, salt) 
+    return passwordHash
+
+# Function to validate that the json received includes all the parameters needed
+def validateData(parameters:list[str],dictionary:dict)->bool:
+    for i in parameters:
+        if i not in dictionary:
+            return False
+    return True
+
+# Function to create the response of the api(mostly used in case of an error)
+def responseJson(code:int, motive:str)->Response:
+    data = {
+        "Status code" : code,
+        "Motive" : motive}
+    response = jsonify(data)
+    response.status_code = code
+    return response
+
+@query(database)
+def checkAdmin(cursor:sqlite3.Cursor,connection:sqlite3.Connection,id:int)->bool:
+    # Retrieve user(will be None in case it's not found)
+    data = cursor.execute("SELECT * FROM users WHERE id = ? AND role = 'admin' ", (id,))
+    row = data.fetchone()
+    if not row:
+        return False
+    return True
+
 #Creates tables needed for the database
-def createTables(connection:sqlite3.Connection):
-    cursor = connection.cursor()
+@query(database)
+def createTables(cursor:sqlite3.Cursor,connection:sqlite3.Connection):
     cursor.execute("""CREATE TABLE IF NOT EXISTS users(
                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 username VARCHAR(50) NOT NULL, 
@@ -36,42 +91,13 @@ def createTables(connection:sqlite3.Connection):
                 proposal_id INT NOT NULL,
                 FOREIGN KEY (user_id) REFERENCES users(id),
                 FOREIGN KEY (proposal_id) REFERENCES proposals(id)
-               )""") 
-    cursor.close()
-
-# Function to hash a password with bcrypt
-def hashPassword(passwordString:str)->str:
-    # Converting password to array of bytes 
-    bytes = passwordString.encode('utf-8') 
-    
-    # Generating the salt 
-    salt = bcrypt.gensalt() 
-    
-    # Hashing the password 
-    passwordHash = bcrypt.hashpw(bytes, salt) 
-    return passwordHash
-
-# Function to validate that the json received includes all the parameters needed
-def validateData(parameters:list[str],dictionary:dict)->bool:
-    for i in parameters:
-        if i not in dictionary:
-            return False
-    return True
-
-#connection = sqlite3.connect('database.db')
-#createTables(connection)
-#connection.close()
-
-app = Flask(__name__)
-
+               )""")     
 
 @app.route('/')
 def index():
     return 'Hello, World!'
 
-# 10.25.240.250
-
-@app.route('/login', methods=["POST","DELETE"])
+@app.route('/login', methods=["POST"])
 def login():
     if request.method == "POST":
         #Login
@@ -79,11 +105,7 @@ def login():
         # Validate data
         jsonData = request.get_json()
         if validateData(["username","password"],jsonData) == False:
-            data = {
-                "Status Code": 400,
-                "Motive":"Incorrect parameters sent"}
-            response = jsonify(data)
-            response.status_code = 400
+            response = responseJson(400,"Incorrect parameters sent")
             return response
         
         # Get the parameters needed for the login
@@ -103,23 +125,13 @@ def login():
             response = jsonify(data)
             response.status_code = 200
             return response
-    elif request.method == "DELETE":
-        # TODO
-        pass
 
 # Function to validate the login info
-def startSession(username,password):
-    # Connect to the database
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-
+@query(database)
+def startSession(cursor:sqlite3.Cursor,connection:sqlite3.Connection,username,password):
     # Query the database for the provided username and password
     cursor.execute("SELECT username,password FROM users WHERE username = ?", (username,))
     user = cursor.fetchone()
-    
-    # Close the database connection
-    cursor.close()
-    connection.close()
 
     
     # If user exists, return True (valid credentials), otherwise return False
@@ -138,63 +150,81 @@ def users():
         # Signup a new user
         jsonData = request.get_json()
         if validateData(["username","password","role","firstname","middlename","lastname"],jsonData) == False:
-            data = {
-                "Status Code": 400,
-                "Motive":"Incorrect parameters sent"}
-            response = jsonify(data)
-            response.status_code = 400
+            response = responseJson(400,"Incorrect parameters sent")
             return response
 
         jsonData["password"] = hashPassword(jsonData["password"])
         if insertUser(jsonData):
             return "",200
         else:
-            data = {
-                "Status Code": 400,
-                "Motive":"User already exists"}
-            response = jsonify(data)
-            response.status_code = 400
+            response = responseJson(400,"User already exists")
             return response
 
 
     elif request.method == "DELETE":
-        id = request.args.get("id")
-        if not id:
-            data = {
-                "Status Code": 400,
-                "Motive":"Incorrect parameters sent"}
-            response = jsonify(data)
-            response.status_code = 400
+        # Delete an existing user
+        jsonData = request.get_json()
+        if validateData(["currentUserId","userId"],jsonData) == False:
+            response = responseJson(400,"Incorrect parameters sent")
             return response
         
-        if deleteUser(id):
+        # Checks if the current user is an admin
+        if not checkAdmin(jsonData["currentUserId"]):
+            response = responseJson(401,"Unauthorized access")
+            return response
+
+        # Checks if the user exists and deletes it
+        if deleteUser(jsonData["userId"]):
             return "",200
         else:
-            data = {
-                "Status Code": 400,
-                "Motive":"User doesn't exist"}
-            response = jsonify(data)
-            response.status_code = 400
+            response = responseJson(400,"User doesn't exist")
             return response
         
     elif request.method == "GET":
-        pass
+        # Get data for a given user
+        id = request.args.get("id")
+        if not id:
+            response = responseJson(400,"Incorrect parameters sent")
+            return response
+        
+        user = getUser(id)
+        if user != None:
+            # Success
+            response = jsonify(user)
+            response.status_code = 200
+            return response
+        else:
+            # User not found
+            response = responseJson(404,"User doesn't exist")
+            return response
+        
     elif request.method == "PUT":
-        pass
+        # Edit existing user
+        jsonData = request.get_json()
+
+        if validateData(["currentUserId","userId","username","password","role","firstname","middlename","lastname","points"],jsonData) == False:
+            response = responseJson(400,"Incorrect parameters sent")
+            return response
+
+        # Checks if the current user is an admin
+        if not checkAdmin(jsonData["currentUserId"]):
+            response = responseJson(401,"Unauthorized access")
+            return response
+
+        jsonData["password"] = hashPassword(jsonData["password"])
+        if editUser(jsonData):
+            return "",200
+        else:
+            response = responseJson(400,"User doesn't exist")
+            return response
         
 # Function to check whether the user exists yet, if it doesn't then it registers the new user
-def insertUser(data:dict):
-    # Connect to the database
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-
+@query(database)
+def insertUser(cursor:sqlite3.Cursor,connection:sqlite3.Connection,data:dict):
     # Checks wheter the user exists
-    cursor.execute("SELECT username,password FROM users WHERE username = ?", (data["username"],))
+    cursor.execute("SELECT * FROM users WHERE username = ?", (data["username"],))
     user = cursor.fetchone()
     if user:
-        # Close the database connection
-        cursor.close()
-        connection.close()
         return False
     
     #Check if points where passed, if not default to 0
@@ -207,29 +237,51 @@ def insertUser(data:dict):
     # Commit the transaction
     connection.commit()
     
-    # Close the database connection
-    cursor.close()
-    connection.close()
     return True
 
-def deleteUser(id:str):
-    # Connect to the database
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-
+# Function to delete user if it exists
+@query(database)
+def deleteUser(cursor:sqlite3.Cursor,connection:sqlite3.Connection,id:str):
     # Checks wheter the user exists
     cursor.execute("SELECT * FROM users WHERE id = ?", (id,))
     user = cursor.fetchone()
     if not user:
-        # Close the database connection
-        cursor.close()
-        connection.close()
         return False
 
     # Borrar al usuario indicado
     cursor.execute("DELETE FROM users WHERE id = ?", (id,))
     connection.commit()
-    # Close the database connection
-    cursor.close()
-    connection.close()
+    return True
+
+# Function that returns user based on id
+@query(database)
+def getUser(cursor:sqlite3.Cursor,connection:sqlite3.Connection,id:str):
+    # Retrieve user(will be None in case it's not found)
+    data = cursor.execute("SELECT id,role,firstname,middlename,lastname,username,points FROM users WHERE id = ?", (id,))
+    row = data.fetchone()
+    if not row:
+        return None
+    
+    # Add keys to the values returned 
+    user = {}
+    for id,column in enumerate(data.description):
+        user[column[0]] = row[id]
+        
+    return user
+
+# Function to check whether the user exists and edits it
+@query(database)
+def editUser(cursor:sqlite3.Cursor,connection:sqlite3.Connection,data:dict):
+    # Checks wheter the user exists
+    cursor.execute("SELECT * FROM users WHERE id = ?", (data["userId"],))
+    user = cursor.fetchone()
+    if not user:
+        return False
+
+    # Insert user
+    cursor.execute("UPDATE users SET username = ?, password = ?, role = ?, firstname = ?, middlename = ?, lastname = ?, points = ? WHERE id = ?",
+                    (data["username"], data["password"], data["role"], data["firstname"], data["middlename"], data["lastname"], data["points"],data["userId"]))
+    
+    # Commit the transaction
+    connection.commit()
     return True
