f3d90636fdad044301912a756cf16155fa8959b5
Giuliana Sofía Islas <81657417+GiulianaIslas@users.noreply.github.com>
2024-06-09 11:25:55 -0600

    fixed login bug

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 document.getElementById('loginForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
-    const username = document.getElementById('usuario').value;
-    const password = document.getElementById('contrasena').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
     try {
         const response = await fetch('http://127.0.0.1:8080/login', {
