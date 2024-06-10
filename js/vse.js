d192c50dd101c0cfc6e2ab3d5ac8cf7124759e3b
rmrhx01 <rmrhx01@gmail.com>
2024-06-09 21:05:48 -0600

    Full functionality for champs and vse pages

--- js/vse.js
+++ js/vse.js
@@ -51,7 +51,7 @@ async function fetchProposals() {
         row.addEventListener('click', function(){
             currentProposalId = proposal.id; 
             const modal = new bootstrap.Modal(document.getElementById('myModal'));
-            ["title", "currentSituation", "description","areaName", "status", "category", "creationDate"].forEach(key => {
+            ["title", "currentSituation", "description","areaName", "status", "category", "creationDate","assignedPoints"].forEach(key => {
                 document.getElementById(key).value = proposal[key];
             });
             document.getElementById('users').value = proposal['users'].join(', ');
@@ -161,61 +161,6 @@ document.addEventListener('DOMContentLoaded',async function(){
             location.reload();
         }
         console.log(response.ok ? 'Mensaje añadido exitosamente' : 'No se pudo mandar el mensaje. Inténtalo de nuevo más tarde.', response.ok ? 'success' : 'error');
-    });
-    
-    document.getElementById('accept').addEventListener('click', async function(){
-        const proposal = await (await fetch(`http://127.0.0.1:8080/proposals?id=${currentProposalId}`)).json();
-        if(document.getElementById('assignedPoints').value == ""){
-            alert('Por favor asigna puntos antes de aceptar la propuesta');
-            return;
-        }
-    
-        let now = new Date();
-    
-        let year = now.getFullYear();
-        let month = (now.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
-        let day = now.getDate().toString().padStart(2, '0');
-    
-        const feedback = JSON.parse(proposal.feedback);
-        const currentMessage = [sessionStorage.getItem('username'),document.getElementById('feedback').value]
-        if(document.getElementById('feedback').value != ""){
-            feedback.push(currentMessage);
-        }
-        const params = {
-            method: 'PUT',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ 
-                currentUser: sessionStorage.getItem('username'),
-                proposalId: currentProposalId,
-                title: proposal['title'],
-                description: proposal['description'],
-                currentSituation: proposal['currentSituation'],
-                area: proposal['area'],
-                status: 'Aprobada',
-                type: proposal['type'],
-                feedback: JSON.stringify(feedback),
-                closeDate:`${year}-${month}-${day}`,
-                category: proposal['category'],
-                assignedPoints: document.getElementById('assignedPoints').value,
-                usersId: proposal['users'],
-                formerEvaluatorUser: proposal['formerEvaluatorUser'],
-                currentEvaluatorUser: proposal['currentEvaluatorUser']
-            })};
-        const response = await fetch('http://127.0.0.1:8080/proposals', params);
-        for(const user of proposal['users']){
-            const userData = await fetchUserById(user);
-            userData['points'] =  parseInt(document.getElementById('assignedPoints').value) + parseInt(userData['points']);
-            userData['username'] = user;
-            userData['currentUser'] = sessionStorage.getItem('username');
-            userData['firstname'] = userData['firstName'];
-            userData['lastname'] = userData['lastName'];
-            userData['middlename'] = userData['middleName'];
-            await handleUserAction('http://127.0.0.1:8080/users', 'PUT', userData);
-        }
-        if(response.ok){
-            location.reload();
-        }
-        
     });
     
     document.getElementById('deny').addEventListener('click', async function(){
